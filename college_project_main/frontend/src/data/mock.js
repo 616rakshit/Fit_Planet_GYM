cf6f6356ef28b8acb9747ca3245871bc321c47e5
RAKSHIT JAIN <jainrakshit616@gmail.com>
2026-04-21 13:06:45 +0530

    Update mock.js

--- college_project_main/frontend/src/data/mock.js
+++ college_project_main/frontend/src/data/mock.js
@@ -148,7 +148,7 @@ export const SITE_DATA = {
     phone: "+91 9650161386 , +91 9821900471",
     email: "fitplanetgym1386@gmail.com",
     hours: " 18/7 Access • Open Every Day",
-    mapEmbed:"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d218.8015043822933!2d77.0996487296325!3d28.664985958644795!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d05f20a05c057%3A0x750b6d6b4f585575!2sFit%20Planet%20Gym!5e0!3m2!1sen!2sin!4v1776756621683!5m2!1sen!2sin"
+    mapEmbed:"https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d218.80123833526045!2d77.09985925439446!3d28.6651133936506!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1776756930527!5m2!1sen!2sin"
   },
 
   testimonials: [
